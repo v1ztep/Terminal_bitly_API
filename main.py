d4508ee10712e5f54090bb575f7bd2463443dff0
Vladimir <v1ztep@gmail.com>
2020-12-04 22:56:12 +0300

    Добавил проверку на неправильную ссылку

--- main.py
+++ main.py
@@ -16,11 +16,15 @@ def shorten_link(token, url):
     response = requests.post('https://api-ssl.bitly.com/v4/shorten',
                              headers=headers, json=data)
     response.raise_for_status()
-    link = json.loads(response.text.replace("'",'"'))["link"]
-    return link
+    bitlink = json.loads(response.text.replace("'",'"'))["link"]
+    return bitlink
 
 def main():
-    link = shorten_link(os.getenv("TOKEN"), input('Введите ссылку:'))
+    try:
+        link = shorten_link(os.getenv("TOKEN"), input('Введите ссылку:'))
+    except requests.exceptions.HTTPError:
+        print('Неправильная ссылка')
+        return
 
     print(link)
 
