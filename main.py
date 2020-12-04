783f36295f1014bc0fe432957e560d9f50305d5d
Vladimir <v1ztep@gmail.com>
2020-12-04 22:33:24 +0300

    Упаковал в функцию shorten_link

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import requests
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 
 URL = "https://github.com/v1ztep"
 
-def main():
+def shorten_link(token, url):
     headers = {
-        'Authorization': f'Bearer {os.getenv("TOKEN")}',
+        'Authorization': f'Bearer {token}',
         'Content-Type': 'application/json',
     }
-    data = '{"long_url": "https://github.com/v1ztep"}'
-
-    response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=data)
+    data = {"long_url": f"{url}"}
+    response = requests.post('https://api-ssl.bitly.com/v4/shorten',
+                             headers=headers, json=data)
     response.raise_for_status()
+    link = json.loads(response.text.replace("'",'"'))["link"]
+    return link
 
-    print(response)
-    print(response.text)
+def main():
+    link = shorten_link(os.getenv("TOKEN"), URL)
+
+    print(link)
 
 if __name__ == '__main__':
     main()
