b3f532fd7659963b4d93677f3ae3030ec322050d
Vladimir <v1ztep@gmail.com>
2020-12-08 18:11:21 +0300

    Использовал встроенный в requests json()

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 from dotenv import load_dotenv
 import os
-import json
 from urllib.parse import urlparse
 
 
@@ -18,7 +17,9 @@ def shorten_link(token, url):
     response = requests.post('https://api-ssl.bitly.com/v4/shorten',
                              headers=headers, json=data)
     response.raise_for_status()
-    bitlink = json.loads(response.text.replace("'",'"'))["link"]
+    bitlink = response.json()["link"]
+    print(bitlink)
+
     return bitlink
 
 def count_clicks(token, bitlink):
@@ -30,7 +31,7 @@ def count_clicks(token, bitlink):
                             headers=headers)
     response.raise_for_status()
 
-    total_clicks = json.loads(response.text.replace("'",'"'))["total_clicks"]
+    total_clicks = response.json()["total_clicks"]
     return total_clicks
 
 def strip_scheme(url):
